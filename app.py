7852d41565ed4072860aef283d2f28fbca284d02
Joboufra <joboufra@gmail.com>
2023-11-12 02:03:00 +0100

    feat: Websockets + PostgreSQL

--- app.py
+++ app.py
@@ -1,46 +1,52 @@
-from flask import Flask, render_template, request, jsonify, Response
-from kafka import KafkaProducer, KafkaConsumer
-from threading import Thread
-import queue
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from config import SQLALCHEMY_DATABASE_URI
+import os
 
+# Configuración inicial de la aplicación Flask y SocketIO
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Productor y consumidor de Kafka
-producer = KafkaProducer(bootstrap_servers=['10.20.10.10:9092'])
-consumer = KafkaConsumer('chat', bootstrap_servers=['10.20.10.10:9092'], auto_offset_reset='earliest')
+db = SQLAlchemy(app)
 
-message_queue = queue.Queue()
+#Modelo de mensajes
+class Message(db.Model):
+    __tablename__ = 'message'
+    __table_args__ = {'schema': 'chat'}
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-def kafka_consumer_thread():
-    for message in consumer:
-        message_queue.put(message.value.decode())
+    def __repr__(self):
+        return f'<Message {self.id}>'
 
-consumer_thread = Thread(target=kafka_consumer_thread, daemon=True)
-consumer_thread.start()
-
-#Obtener mensajes
-def event_stream():
-    while True:
-        message = message_queue.get()
-        yield f"data: {message}\n\n"
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('chat.html')
+    last_messages = Message.query.order_by(Message.timestamp.desc()).limit(10).all()
+    last_messages = last_messages[::-1]
+    return render_template('chat.html', messages=last_messages)
 
-@app.route('/send', methods=['POST'])
-def send_message():
-    data = request.json
-    message = data.get('message')
-    if message:
-        producer.send('chat', message.encode())
-        return jsonify({'status': 'Mensaje enviado'}), 200
-    else:
-        return jsonify({'error': 'No se proporcionó mensaje'}), 400
 
-@app.route('/events')
-def sse():
-    return Response(event_stream(), mimetype="text/event-stream")
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    message_text = data.get('message')
+    if message_text:
+        #Crear y almacenar el mensaje en la base de datos
+        message = Message(message=message_text)
+        db.session.add(message)
+        db.session.commit()
+
+        #Emitir el mensaje a todos los clientes conectados
+        time_stamp = message.timestamp.strftime('%H:%M:%S')
+        emit('receive_message', {'message': message_text, 'time': time_stamp}, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    socketio.run(app, debug=True)
--- static/script.js
+++ static/script.js
@@ -6,55 +6,30 @@ document.getElementById('message-input').addEventListener('keypress', function (
   }
 });
 
-function clearMessages() {
-  const messagesDiv = document.getElementById('messages');
-  messagesDiv.innerHTML = ''; // Limpiar chat
-  localStorage.removeItem('viewedMessages'); // Limpiar LocalStorage
-}
+const socket = io(); 
 
+socket.on('receive_message', function(data) {
+  addMessage(data.message, data.time);
+});
+
+//Funciones
 function sendMessage() {
   const messageInput = document.getElementById('message-input');
   const message = messageInput.value;
-  messageInput.value = ''; // Limpiar input
+  messageInput.value = '';
 
-  fetch('/send', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message: message }),
-  });
+  socket.emit('send_message', { message: message });
 }
 
-const eventSource = new EventSource('/events');
-eventSource.onmessage = function (event) {
+function addMessage(message, time) {
   const messagesDiv = document.getElementById('messages');
-  let viewedMessages = JSON.parse(localStorage.getItem('viewedMessages')) || [];
-  const msg = event.data;
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = `<span class="message-time">${time}</span>: ${message}`;
+  messagesDiv.appendChild(messageElement); 
+}
 
-  if (!viewedMessages.some(obj => obj.message === msg)) {
-      const messageElement = document.createElement('div');
-      const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-      messageElement.innerHTML = `<span class="message-time">${currentTime}</span>: ${msg}`;
-      messagesDiv.appendChild(messageElement);
-      viewedMessages.push({ message: msg, time: currentTime });
-
-      // Convertir los objetos a cadenas JSON antes de almacenarlos
-      viewedMessages = JSON.stringify(viewedMessages);
-      localStorage.setItem('viewedMessages', viewedMessages);
-
-      // Volver a convertir las cadenas JSON a objetos antes de mostrarlos
-      viewedMessages = JSON.parse(viewedMessages);
-  }
-};
-
-window.addEventListener('load', () => {
-  const viewedMessages = JSON.parse(localStorage.getItem('viewedMessages')) || [];
+function clearMessages() {
   const messagesDiv = document.getElementById('messages');
+  messagesDiv.innerHTML = '';
+}
 
-  viewedMessages.forEach(obj => {
-      const messageElement = document.createElement('div');
-      messageElement.innerHTML = `<span class="message-time">${obj.time}</span>: ${obj.message}`;
-      messagesDiv.appendChild(messageElement);
-  });
-});
